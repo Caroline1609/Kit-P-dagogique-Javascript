26baa415919f9933df9aa99e05d5c2cf40cd10a4
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-07 12:53:43 +0200

    upload

---
+++ Manipuler les Styles/asset/js/script.js
@@ -0,0 +1,12 @@
+const AugmenterTaille = document.getElementById('AugmenterTaille').style.fontSize="16px";
+const texte = document.getElementById('texte').style.fontSize="16px";
+
+console.log(AugmenterTaille);
+
+
+AugmenterTaille.addEventListener("click", event => {
+    
+    var texte = document.getElementById('texte');
+    
+    
+})
