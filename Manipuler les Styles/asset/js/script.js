843cf2ed3a98b620ad40ffcce840e852515852ca
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-07 16:55:19 +0200

    upload

--- Manipuler les Styles/asset/js/script.js
+++ Manipuler les Styles/asset/js/script.js
@@ -1,12 +1,30 @@
-const AugmenterTaille = document.getElementById('AugmenterTaille').style.fontSize="16px";
-const texte = document.getElementById('texte').style.fontSize="16px";
+const augmenterTaille = document.getElementById('AugmenterTaille');
+const taille = document.getElementById('taille');
+const texte = document.getElementById('texte');
+
+let tailleParDefaut = 16; // Variable qui stocke la taille actuelle de la police, initialisée à 16.
 
 console.log(AugmenterTaille);
 
 
-AugmenterTaille.addEventListener("click", event => {
+augmenterTaille.addEventListener("click", function() {
     
-    var texte = document.getElementById('texte');
-    
-    
+    if (tailleParDefaut < 48) {
+
+        //Compteur
+        let compteur = augmenterTaille.innerText;
+        compteur = parseInt(compteur);
+        compteur++;
+        augmenterTaille.innerText = compteur;
+
+        //Augmenter la taille
+        tailleParDefaut+= 1
+        texte.style.fontSize = tailleParDefaut;
+        taille.textContent = tailleParDefaut; //affiche l'incrédentation de la taille
+    } else {
+
+    tailleParDefaut = 16;
+    texte.style.fontSize = tailleParDefaut;
+    taille.textContent = tailleParDefaut;
+    }
 })
