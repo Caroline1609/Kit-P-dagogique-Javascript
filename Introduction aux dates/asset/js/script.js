884a12c3a3197f905565fe3c6c254e67234af3f5
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-10 16:47:01 +0200

    upload

--- Introduction aux dates/asset/js/script.js
+++ Introduction aux dates/asset/js/script.js
@@ -22,7 +22,7 @@ calculer.addEventListener('click', function() {
 
     } else {
         formatageDate(dateUtilisateur, actuelle);
-        signeAstrologique(date);
+        signeAstrologique(dateUtilisateur);
         
     }
 
@@ -33,19 +33,44 @@ function formatageDate(dateUtilisateur, actuelle){
     const formatageFinal = dateUtilisateur.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
     const anneePasse = actuelle.getFullYear() - dateUtilisateur.getFullYear();
 
-    resultat.innerHTML = `Vous êtes né(e) le ${formatageDate} à ${formatageFinal}.`;
+    resultat.innerHTML = `Vous êtes né(e) le <span class= "color">${formatageDate} </span> à <span class= "color">${formatageFinal}</span>.`;
     dateEcouler.innerHTML = `Il s'est écoulé ${anneePasse} années depuis votre naissance.`;
-    signeAstro.innerHTML = ` Votre signe astrologique est : ${signe}.`;
+    
 }
 
 function signeAstrologique(date) {
     const jour = date.getDate();
     const mois = date.getMonth() + 1; // Les mois sont indexés à partir de 0
 
+
     const signes = [
     { nom: "Verseau", debut: { mois: 1, jour: 20 }, fin: { mois: 2, jour: 18 } },
-    { nom: "Poissons", debut: { mois: 1, jour: 20 }, fin: { mois: 2, jour: 18 } },
-
-
+    { nom: "Poissons", debut: { mois: 2, jour: 19 }, fin: { mois: 3, jour: 20 } },
+    { nom: "Belier", debut: { mois: 4, jour: 21 }, fin: { mois: 4, jour: 19 } },
+    { nom: "Taureau", debut: { mois: 4, jour: 20 }, fin: { mois: 5, jour: 20 } },
+    { nom: "Gemeaux", debut: { mois: 5, jour: 21 }, fin: { mois: 6, jour: 20 } },
+    { nom: "Cancer", debut: { mois: 6, jour: 21 }, fin: { mois: 7, jour: 22 } },
+    { nom: "Lion", debut: { mois: 7, jour: 23 }, fin: { mois: 8, jour: 22 } },
+    { nom: "Vierge", debut: { mois: 8, jour: 23 }, fin: { mois: 9, jour: 22 } },
+    { nom: "Balance", debut: { mois: 9, jour: 23 }, fin: { mois: 10, jour: 22 } },
+    { nom: "Scorpion", debut: { mois: 10, jour: 23 }, fin: { mois: 11, jour: 21 } },
+    { nom: "Sagittaire", debut: { mois: 11, jour: 22 }, fin: { mois: 12, jour: 21 } },
+    { nom: "Capricorne", debut: { mois: 12, jour: 22 }, fin: { mois: 1, jour: 19 } },
     ]
+
+    const signe = signes.find(signe => {
+        if (mois === signe.debut.mois && jour >= signe.debut.jour) {
+            return true;
+        } else if (mois === signe.fin.mois && jour <= signe.fin.jour) {
+            return true;
+        }
+        return false;
+    });
+
+    if (signe) {
+        signeAstro.innerHTML = `Votre signe astrologique est :  <span class= "color">${signe.nom}</span>.`;
+    } else {
+        signeAstro.innerHTML = "Impossible de déterminer votre signe astrologique.";
+    }
 }
+
