05f66fe420008708bb1e557bda056fc98bc8229a
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-11 09:32:04 +0200

    upload

--- Introduction aux dates/asset/js/script.js
+++ Introduction aux dates/asset/js/script.js
@@ -5,14 +5,17 @@ const signeAstro = document.getElementById("signeAstro");
 
 
 
+
 calculer.addEventListener('click', function() {
 
     const input = document.getElementById("dateDeNaissance").value;
+        const actuelle = new Date();
     
     const dateUtilisateur = new Date(input);
-    const actuelle = new Date();
-  
-    if (!input) {
+    if(isNaN(dateUtilisateur.valueOf())){
+         resultat.innerHTML = "Erreur";
+    }   
+    else if (!input) {
 
         resultat.innerHTML = "Veuillez entrer une date.";
 
@@ -28,13 +31,30 @@ calculer.addEventListener('click', function() {
 
 });
 
+function anneBisextile(annee) {
+    return (annee % 4 === 0 && annee % 100 !== 0) || (annee % 400 === 0);
+}
+
 function formatageDate(dateUtilisateur, actuelle){
     const formatageDate = dateUtilisateur.toLocaleDateString('fr-FR');
     const formatageFinal = dateUtilisateur.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
-    const anneePasse = actuelle.getFullYear() - dateUtilisateur.getFullYear();
+    const isLeapYear = (year) => new Date(year, 1, 29).getMonth() === 1;
 
-    resultat.innerHTML = `Vous êtes né(e) le <span class= "color">${formatageDate} </span> à <span class= "color">${formatageFinal}</span>.`;
-    dateEcouler.innerHTML = `Il s'est écoulé ${anneePasse} années depuis votre naissance.`;
+    const anneePasse = actuelle.getFullYear() - dateUtilisateur.getFullYear();
+    const anneeNaissance = dateUtilisateur.getFullYear();
+    const estBissextile = isLeapYear(anneeNaissance);
+    
+    
+    if (estBissextile){
+        resultat.innerHTML = `Erreur de date recommencer`;
+
+    } else{
+        resultat.innerHTML = `Vous êtes né(e) le <span class= "color">${formatageDate} </span> à <span class= "color">${formatageFinal}</span>.`;
+        dateEcouler.innerHTML = `Il s'est écoulé ${anneePasse} années depuis votre naissance.`;
+    }
+    
+
+    
     
 }
 
@@ -46,9 +66,9 @@ function signeAstrologique(date) {
     const signes = [
     { nom: "Verseau", debut: { mois: 1, jour: 20 }, fin: { mois: 2, jour: 18 } },
     { nom: "Poissons", debut: { mois: 2, jour: 19 }, fin: { mois: 3, jour: 20 } },
-    { nom: "Belier", debut: { mois: 4, jour: 21 }, fin: { mois: 4, jour: 19 } },
+    { nom: "Bélier", debut: { mois: 4, jour: 21 }, fin: { mois: 4, jour: 19 } },
     { nom: "Taureau", debut: { mois: 4, jour: 20 }, fin: { mois: 5, jour: 20 } },
-    { nom: "Gemeaux", debut: { mois: 5, jour: 21 }, fin: { mois: 6, jour: 20 } },
+    { nom: "Gémeaux", debut: { mois: 5, jour: 21 }, fin: { mois: 6, jour: 20 } },
     { nom: "Cancer", debut: { mois: 6, jour: 21 }, fin: { mois: 7, jour: 22 } },
     { nom: "Lion", debut: { mois: 7, jour: 23 }, fin: { mois: 8, jour: 22 } },
     { nom: "Vierge", debut: { mois: 8, jour: 23 }, fin: { mois: 9, jour: 22 } },
