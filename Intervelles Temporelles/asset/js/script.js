eb9aca96e081c5c23a581b01eb9e5e11dd3a71b6
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-15 15:59:24 +0200

    upload

--- Intervelles Temporelles/asset/js/script.js
+++ Intervelles Temporelles/asset/js/script.js
@@ -3,23 +3,35 @@ const heureActuelle = document.getElementById("heureActuelle");
 const bouton1 = document.getElementById("boutonDate");
 const resultat = document.getElementById("resultatDateHeure");
 
+
+function ajout0(nombre){
+    if(nombre < 10){
+        nombre = "0" + nombre;  
+    } 
+    return nombre;
+}
+
 bouton1.addEventListener("click", function() {
 
-    const maintenant = new Date();
 
-    
+    const date1 = document.getElementById('dateActuelle').value;
+    const heure = document.getElementById('heureActuelle').value;
+    const dateSaisie = new Date(date1 + " " +  heure);
 
-    const jour = maintenant.getDate();
-    const mois = maintenant.getMonth() + 1;
-    const annee = maintenant.getFullYear();
+    const jour = dateSaisie.getDate();
+    const mois = dateSaisie.getMonth() + 1;
+    const annee = dateSaisie.getFullYear();
 
-    const heures = maintenant.getHours();
-    const minutes = maintenant.getMinutes();
+    const heures = dateSaisie.getHours();
+    const minutes = dateSaisie.getMinutes();
 
-    const dateFormatee = `${jour}/${mois}/${annee}`;
-    const heureFormatee = `${heures}:${minutes}`;
+    const dateFormatee = `${ajout0(jour)}/${ajout0(mois)}/${annee}`;
+    const heureFormatee = `${ajout0(heures)}:${ajout0(minutes)}`;
 
-    console.log(maintenant)
 
-    resultat.textContent = `Aujourd'hui, nous sommes le ${dateFormatee} et il est ${heureFormatee}.`;
+    resultat.innerHTML = `Aujourd'hui, nous sommes le <span class="blue">${dateFormatee}</span> et il est <span class="blue">${heureFormatee}</span>.`;
 });
+
+function date2(){
+    
+}
