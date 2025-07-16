b116c270afd7f32989f8c8b5363d1ba0678fe12c
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-16 12:55:17 +0200

    upload

--- Intervelles Temporelles/asset/js/script.js
+++ Intervelles Temporelles/asset/js/script.js
@@ -1,56 +1,47 @@
-const dateActuelle = document.getElementById("dateActuelle");
+let dateActuelle = document.getElementById("dateActuelle");
 const heureActuelle = document.getElementById("heureActuelle");
 const bouton1 = document.getElementById("boutonDate");
 const resultat = document.getElementById("resultatDateHeure");
 
 
 
-
-bouton1.addEventListener("click", function() {
-
+function afficherDate(){
 
     const date1 = document.getElementById('dateActuelle').value;
     const heure = document.getElementById('heureActuelle').value;
-    const dateSaisie = new Date(date1 + " " +  heure);
 
-    const jour = dateSaisie.getDate();
-    const mois = dateSaisie.getMonth() + 1;
-    const annee = dateSaisie.getFullYear();
+    const dateSaisie = new Date(date1 + " " + heure);
 
-    const heures = dateSaisie.getHours();
-    const minutes = dateSaisie.getMinutes();
-    const secondes = dateSaisie.getSeconds();
+    const optionsDate = { day: '2-digit', month: '2-digit', year: 'numeric' };
+    const optionsHeure = { hour: '2-digit', minute: '2-digit', second: '2-digit'};
 
-    const dateFormatee = `${ajout0(jour)}/${ajout0(mois)}/${annee}`;
-    const heureFormatee = `${ajout0(heures)}:${ajout0(minutes)}:${ajout0(secondes)}`;
-
+    const dateFormatee = dateSaisie.toLocaleDateString("fr-FR", optionsDate);
+    const heureFormatee = dateSaisie.toLocaleTimeString("fr-FR", optionsHeure);
 
     resultat.innerHTML = `Aujourd'hui, nous sommes le <span class="blue">${dateFormatee}</span> et il est <span class="blue">${heureFormatee}</span>.`;
-
-
-    
-});
-
-function ajout0(nombre){
-    if(nombre < 10){
-        nombre = "0" + nombre;  
-    } 
-    return nombre;
 }
 
-function formatDateTime(date){
+
+bouton1.addEventListener("click", afficherDate);
+
+function calculerIntervalle(){
+    const input = document.getElementById('dateAutre');
+
+    const saisieInput = new Date(input);
 
 
-    const jour = date.getDate();
-    const mois = date.getMonth() + 1;
-    const annee = date.getFullYear();
+    let dateDiff = saisieInput - dateSaisie;
+    dateDiff = dateDiff / 1000 / 60 / 60 / 24 / 365.25; // convertit millescondes en années
+    dateDiff = Math.floor(dateDiff); // on conserve la partie entière du nombre, on aurait pu utiliser "parseInt(dateDiff)"
 
+resultatDateHeure2.innerHTML = `Il y a ${dateDiff} et il est <span class="blue">${heureFormatee}</span>.`;
 
-    const heures = date.getHours();
-    const minutes = date.getMinutes();
-    const secondes = date.getSeconds();
-
-    const dateFormat = `${ajout0(jour)}/${ajout0(mois)}/${annee}`;
-    const heureFormat = `${ajout0(heures)}:${ajout0(minutes)}:${ajout0(secondes)}`;
 }
+
+
+
+
+
+
+
 
