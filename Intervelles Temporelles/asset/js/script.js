63b80ecf307d394c2c0842006e657bdbc87822bd
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-17 15:19:37 +0200

    upload

--- Intervelles Temporelles/asset/js/script.js
+++ Intervelles Temporelles/asset/js/script.js
@@ -27,25 +27,41 @@ function afficherDate() {
 }
 
 function dateDiffInDays(date2, date1) {
-    const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    return diffDays;
+    const diffTime = date2 - date1;
+
+    // Calculer les jours
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+    // Calculer le reste en heures
+    const diffHours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    // Calculer le reste en minutes
+    const diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+
+    // Calculer le reste en secondes
+    const diffSeconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+
+    return { jours: diffDays, heures: diffHours, minutes: diffMinutes, secondes: diffSeconds };
 }
 
 function calculerIntervalle() {
-    
     const date1 = new Date(dateActuelle.value + " " + heureActuelle.value);
     const input = dateAutre.value;
     const saisieInput = new Date(input);
-
     const optionsDate = { day: '2-digit', month: '2-digit', year: 'numeric' };
     const optionsHeure = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
-
     const dateFormatee = saisieInput.toLocaleDateString("fr-FR", optionsDate);
     const heureFormatee = saisieInput.toLocaleTimeString("fr-FR", optionsHeure);
 
-    let jours = dateDiffInDays(saisieInput, date1);
-    resultatDateHeure2.innerHTML = `Il y a ${jours} jours entre aujourd'hui et le <span class= "blue"> ${dateFormateeGlobale} à ${heureFormateeGlobale} </span>.`;
+    const { jours, heures, minutes, secondes } = dateDiffInDays(saisieInput, date1);
+
+    if (jours > 0) {
+        resultatDateHeure2.innerHTML = `Dans ${jours} jours, ${heures} heures et ${minutes} minutes, nous serons le <span class="blue">${dateFormatee} à ${heureFormatee}</span>.`;
+    } else if (jours < 0) {
+        resultatDateHeure2.innerHTML = `Il y a ${Math.abs(jours)} jours, ${Math.abs(heures)} heures et ${Math.abs(minutes)} minutes, nous étions le <span class="blue">${dateFormatee} à ${heureFormatee}</span>.`;
+    } else {
+        resultatDateHeure2.innerHTML = `Aujourd'hui, nous sommes le <span class="blue">${dateFormatee} à ${heureFormatee}</span>.`;
+    }
 }
 
 bouton1.addEventListener("click", afficherDate);
