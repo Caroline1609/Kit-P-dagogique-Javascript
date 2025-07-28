e582b5240b456b6bf301b2d389225fa226f449d1
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-28 09:12:14 +0200

    upload

--- Tableau Objet/asset/js/script.js
+++ Tableau Objet/asset/js/script.js
@@ -1,36 +1,61 @@
+const form = document.getElementById("form");
+const identifiant = document.getElementById("identifiant");
+const passwordInput = document.getElementById("motdepasse");
+const message = document.getElementById("message");
+const button = document.getElementById("button");
+
+  let users = [];
 
 // Charger les utilisateurs depuis le fichier JSON
 fetch("./asset/json/user.json")
   .then(response => response.json())
   .then(data => {
     users = data; // on stocke les utilisateurs récupérés
+    console.log("Utilisateurs chargés :", users);
   })
   .catch(error => {
     console.error("Erreur lors du chargement du fichier JSON :", error); // Gérer les erreurs de chargement
   });
 
-// Gérer la connexion au clic sur le bouton
-document.querySelector("form").addEventListener("click", connexion);
+
+  function messageError(messageText){
+    const message = document.getElementById("message");
+
+    message.textContent = messageText;
+    message.classList.add('active');
+    /*message.style.border = "1px solid #999B86";
+    message.style.backgroundColor  = "#C1AE9F";
+    message.style.padding= "20px";
+    message.style.alignContent = "center";
+    message.style.color = "#886174";*/
+    
+    setTimeout(function() {
+      message.textContent = '';
+      message.classList.remove('active');
+    }, 5000);
+  }
+
+
+
 
 
   function connexion(){
-    const identifiantInput = document.getElementById("identifiant").value.trim().toLowerCase();
-  const passwordInput = document.getElementById("motdepasse").value;
-  const message = document.getElementById("message");
+    const identifiantsaisie = identifiant.value.trim().toLowerCase();
+    const motDePasseSaisi = passwordInput.value;
 
-
-  
-
-  const user = users.find(user => {
+    const user = users.find(user => {
     const identifiant = `${user.firstname.toLowerCase()}.${user.lastname.toLowerCase()}`;
-    return identifiant === identifiantInput && user.password === passwordInput;
+    return identifiant === identifiantsaisie && user.password === motDePasseSaisi;
   });
 
   if (user) {
-    message.textContent = `✅ Bienvenue ${user.firstname} ${user.lastname} !`;
-    message.style.color = "green";
+    message.textContent = `Bienvenue ${user.firstname} ${user.lastname} !`;
+
   } else {
-    message.textContent = "❌ Identifiant ou mot de passe incorrect.";
-    message.style.color = "red";
+
+    messageError("Identifiant ou mot de passe incorrect");
+
   }
-  }
+}
+
+  button.addEventListener("click", connexion);
