4d7a1e33ad315b93ac7fb9d39d59d9b100d0241d
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-07-28 16:48:18 +0200

    upl

--- CodePostal/asset/js/script.js
+++ CodePostal/asset/js/script.js
@@ -6,13 +6,12 @@ const resultat = document.getElementById("resultatVille");
 let zipcodesData = [];
 
   // Chargement du fichier JSON avec fetch
-  fetch("./CodePostal/asset/json/zipcodes.json")
+  fetch("./asset/json/zipcodes.json")
     .then(response => response.json())
     .then(data => {
     
       zipcodesData = data;
+      console.log("Données chargées avec succès :", zipcodesData);
     });
-
-console.log("Données chargées avec succès :", zipcodesData);
 
   
